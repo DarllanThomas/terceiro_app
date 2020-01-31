bad8d3ff719d980b2649d69db565dea86f538881
Darllan Thomas <darllanthomas@hotmail.com>
2020-01-31 16:16:19 -0200

    Finalizar páginas estáticas

---
+++ app/controllers/paginas_estaticas_controller.rb
@@ -0,0 +1,14 @@
+class PaginasEstaticasController < ApplicationController
+  def inicio
+    "Criar controlador para páginas estáticas"
+  end
+   
+
+  def ajuda
+    "Criar controlador para páginas estáticas"
+  end
+
+  def sobre
+    "Criar controlador para páginas estáticas"
+  end
+end
